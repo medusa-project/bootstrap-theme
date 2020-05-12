7c4014c97c8d442d433ace18c3b3ccbab873a70e
Alex Dolski <alexd@n-ary.net>
2020-05-12 16:02:47 -0500

    Add images to app.config.assets.precompile

--- lib/scars-bootstrap-theme/rails/engine.rb
+++ lib/scars-bootstrap-theme/rails/engine.rb
@@ -1,6 +1,10 @@
 module ScarsBootstrapTheme
   module Rails
     class Engine < ::Rails::Engine
+      initializer "scars-bootstrap-theme.assets.precompile" do |app|
+        puts Dir[__dir__ + '/../../../app/assets/images/*']
+        app.config.assets.precompile += Dir[__dir__ + '/../../../app/assets/images/*']
+      end
     end
   end
 end
